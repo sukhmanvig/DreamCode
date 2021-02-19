835a2adcb89f9cd6ca2949dc4b19bddf6a2f6edc
Mulliganaceous <mulliganaceous@outlook.com>
2021-02-19 17:22:09 -0500

    OMG

---
+++ src/quest.php
@@ -0,0 +1,16 @@
+<?php
+
+class Quest {
+	public $xp = 0;
+	public $level = 0;
+	public $view = "";
+
+	public function __construct($view) {
+        $this->$view = view;
+    }
+	
+	public function gotoview($guess){
+		
+	}
+}
+?>
