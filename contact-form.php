ad0898844db26e682ba6de699631d61d22df4b39
Jazelle Diaz <jazelle.diaz@mail.utoronto.ca>
2021-02-25 21:40:44 -0500

    [DREAM-72] Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -2,10 +2,35 @@
 	$name = $email = $subject = $message = "";
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	  $name = test_input($_POST["name"]);
-	  $email = test_input($_POST["email"]);
-	  $website = test_input($_POST["website"]);
-	  $comment = test_input($_POST["comment"]);
-	  $gender = test_input($_POST["gender"]);
+		
+			
+		$name = $_POST["name"];
+		$mailFrom  = $_POST["email"];
+		$subject = $_POST["subject"];
+		$message = $_POST["message"];
+
+		if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
+		  $nameErr = "Only letters and white space allowed";
+		}
+		if (!filter_var($mailFrom, FILTER_VALIDATE_EMAIL)) {
+		  $emailErr = "Invalid email format";
+		}
+		
+		// DreamTeam email
+		$mailTo = "jzeldiaz@gmail.com";
+		$headers = "From: ".$mailFrom;
+		$alertMsg = "You've got mail from ".$name.".\n\n".$message;
+		
+//		echo "You've got mail!\n";
+//		echo "From: ".$name."\nEmail: ".$mailFrom."\nSubject: ".$subject."\nMessage: ".$message;
+		
+		// to, subj, msg, headers
+		mail($mailTo, $subject, $message, $headers);
+		header("Location: contact.html?mailsent");
+		
+		
 	}
+
+
+
 ?>
