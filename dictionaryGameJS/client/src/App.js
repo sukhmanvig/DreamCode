9629b21ebbcf03eb92e0dd909c14cdf76b07da07
Jayvin Chang <jayvin.chang@mail.utoronto.ca>
2021-03-22 05:29:57 +0000

    [DREAM-31] Add dictionary code snippets and explanations

---
+++ dictionaryGameJS/client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+import DictionaryCodeSnippets from './components/dictcode.js'
+
+function App() {
+  return (
+    <Fragment>
+      <DictionaryCodeSnippets />
+    </Fragment>
+  );
+}
+
+export default App;
