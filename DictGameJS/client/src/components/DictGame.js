3f4283e13c972f54e5b951d8dcfe745d6597942c
Jayvin Chang <jayvin.chang@mail.utoronto.ca>
2021-03-25 07:01:03 +0000

    [DREAM-37] Implement code display section

--- DictGameJS/client/src/components/DictGame.js
+++ DictGameJS/client/src/components/DictGame.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import '../css/dictgame-style.css';
 import '../css/nav.css';
-import { GameSetup, GenerateDict } from './DictGameLogic.js';
+import { GameSetup, GenerateDict, CheckIfWon, GameRestart } from './DictGameLogic.js';
 
 var MAX_NODES = 4;
 var MIN_NODES = 1;
@@ -15,7 +15,11 @@ const DictGame = () => {
 
 		numNodes = Math.floor(Math.random()*(MAX_NODES-MIN_NODES)) + MIN_NODES;
 		GenerateDict(document.querySelector("#solDictContainer"), numNodes);
-	});
+
+		if (CheckIfWon()) {
+			GameRestart();
+		}
+	}, []);
 
     return(
         <Fragment>
@@ -23,19 +27,35 @@ const DictGame = () => {
 	    	<div id="dictgame_content">
            		<h1 className="dictgame_h1">Dictionary Game</h1>
 	    		<div id="gameWindow">
-				<div className="dictOuterContainer">
-	    				<h2 className="dictgame_h2">Target Dictionary</h2>
-					<div id="solDictContainer">
-	    					 <div id="dictKeys">
-						 	<h3 className="dictgame_h3">Keys</h3>
-	    					 </div>
-	    					 <div id="dictValues">
-	    		 				<h3 className="dictgame_h3">Values</h3>
-	    		 		 	 </div>
-	   				</div>
+				<div id="gameDescription">
+	    				<h2 className="dictgame_h2">Goal</h2>
+	    				<p>Add and remove key-value pairs from the dictionary <strong>myDict</strong> so that it</p>
+	    				<p> matches the dictionary <strong>targetDict</strong>.</p>
+	    				<div id="gameStatus"></div>
+	    				<button className="dictGameButton" id="toDictTutorial">Return to Tutorial</button>
+	    				<button className="dictGameButton" id="dictShowHideCode">Show/Hide Code</button>
+	    				<button className="dictGameButton" id="dictPlayAgain">Play Again</button>
+	    				<div id="gameActions">
+						<div id="dictModify">
+	    						<h2 className="dictgame_h2">Dictionary Add/Update</h2>
+	    						<label htmlFor="dictGameSelectAddKey">Key</label>
+	    						<select id="dictGameSelectAddKey"></select>
+	    						<label htmlFor="dictGameSelectAddValue">Value</label>
+	    						<select id="dictGameSelectAddValue"></select>
+	    						<div id="dictModifyStatus"></div>
+	    						<button className="dictGameButton" id="dictGameButtonModify">Add Key and Value to Dictionary</button>
+	   					</div>
+						<div id="dictRemove">
+	    						<h2 className="dictgame_h2">Dictionary Remove</h2>
+	    						<label htmlFor="dictGameSelectRemoveKey">Key</label>
+	    						<select id="dictGameSelectRemoveKey"></select>
+	    						<div id="dictRemoveStatus"></div>
+	    						<button className="dictGameButton" id="dictGameButtonRemove">Remove Key From Dictionary</button>
+	   					</div>
+					</div>
 	    			</div>
-	    			<div className="dictOuterContainer">
-	    				<h2 className="dictgame_h2">Your Dictionary</h2>
+				<div className="dictOuterContainer">
+	    				<h2 className="dictgame_h2">myDict</h2>
 		 			<div id="dictContainer">
 	    				        <div id="dictKeys">
 			 				<h3 className="dictgame_h3">Keys</h3>
@@ -45,23 +65,21 @@ const DictGame = () => {
 	    			 		</div>
 	    				</div>
 	    			</div>
-	    			<div id="dictModify">
-	    				<h2 className="dictgame_h2">Dictionary Add/Update</h2>
-	    				<label htmlFor="dictGameSelectAddKey">Key</label>
-	    				<select id="dictGameSelectAddKey"></select>
-	    				<label htmlFor="dictGameSelectAddValue">Value</label>
-	    				<select id="dictGameSelectAddValue"></select>
-	    				<button className="dictGameButtonUpdate">Add Key and Value to Dictionary</button>
-	   			</div>
-				<div id="dictRemove">
-	    				<h2 className="dictgame_h2">Dictionary Remove</h2>
-	    				<label htmlFor="dictGameSelectRemoveKey">Key</label>
-	    				<select id="dictGameSelectRemoveKey"></select>
-	    				<label htmlFor="dictGameSelectRemoveValue">Value</label>
-	    				<select id="dictGameSelectRemoveValue"></select>
-	    				<button className="dictGameButtonRemove">Remove Key and Value From Dictionary</button>
-	   			</div>
-
+				<div className="dictOuterContainer">
+	    				<h2 className="dictgame_h2">targetDict</h2>
+					<div id="solDictContainer">
+	    					 <div id="dictKeys">
+						 	<h3 className="dictgame_h3">Keys</h3>
+	    					 </div>
+	    					 <div id="dictValues">
+	    		 				<h3 className="dictgame_h3">Values</h3>
+	    		 		 	 </div>
+	   				</div>
+	    			</div>
+				<div id="dictCode">
+              				<h2 className="dictgame_h2">Code For Actions</h2>
+              				<div id="dictCodeOutput"></div>
+	    			</div>
 	    		</div>
 	   	</div>
 	   </body>
--- DictGameJS/client/src/components/DictGameLogic.js
+++ DictGameJS/client/src/components/DictGameLogic.js
@@ -1,17 +1,19 @@
 var nodeTypes = ['square', 'triangle', 'circle'];
+var MAX_CODE_LINES = 12;
 
 const GameSetup = () => {
 	//Add event listeners
-	document.querySelector(".dictGameButtonUpdate").addEventListener('click', updateKeyValue);
-	document.querySelector(".dictGameButtonRemove").addEventListener('click', removeKeyValue);
+	document.querySelector("#dictGameButtonModify").addEventListener('click', modifyKeyValue);
+	document.querySelector("#dictGameButtonRemove").addEventListener('click', removeKey);
+	document.querySelector("#dictPlayAgain").addEventListener('click', GameRestart);
+	document.querySelector("#dictShowHideCode").addEventListener('click', ChangeCodeVisibility);
 
 	//Prepopulate select dropdowns
 	var addKeySelect = document.querySelector("#dictGameSelectAddKey");
 	var addValueSelect = document.querySelector("#dictGameSelectAddValue");
 	var removeKeySelect = document.querySelector("#dictGameSelectRemoveKey");
-	var removeValueSelect = document.querySelector("#dictGameSelectRemoveValue");
 
-	var selectLists = [addKeySelect, addValueSelect, removeKeySelect, removeValueSelect];
+	var selectLists = [addKeySelect, addValueSelect, removeKeySelect];
 	var i, j = 0;
 	var option, selectList = "";
 
@@ -22,13 +24,19 @@ const GameSetup = () => {
 		for (j = 0; j < nodeTypes.length; j++) {
 			option = document.createElement('option');
 			option.value = nodeTypes[j];
-			option.innerHTML = nodeTypes[j];
+			option.innerHTML = nodeTypes[j].charAt(0).toUpperCase() + nodeTypes[j].slice(1);
 			selectList.appendChild(option);
 		}
 	}
 }
 
+//Restart the dictionary game
+const GameRestart = () => {
+	//reload window
+	window.location.reload();
+}
 
+//Generate a dictionary
 const GenerateDict = (dictCtr, numNodes) => {    
    
     var i, j, rand, node = 0;
@@ -79,25 +87,36 @@ const createNode = type => {
     return imgNode;
 }
 
-const updateKeyValue = () => {
+//Add/update key-value in the user dictionary
+const modifyKeyValue = () => {
+	clearGameStatus();
+
+	//Get user selected values
 	var keySelect = document.querySelector("#dictGameSelectAddKey");
 	var valSelect = document.querySelector("#dictGameSelectAddValue");
 
-	var keyClass = keySelect.options[keySelect.selectedIndex].value; //key to add
-	var valClass = valSelect.options[valSelect.selectedIndex].value; //value to add
+	var keyClass = keySelect.options[keySelect.selectedIndex].value.toLowerCase(); //key to add
+	var valClass = valSelect.options[valSelect.selectedIndex].value.toLowerCase(); //value to add
 	
+	//Dictionary Columns
 	var keyColumn = document.querySelector("#dictContainer #dictKeys");
 	var valColumn = document.querySelector("#dictContainer #dictValues");  
 
+	//Clear error messages
+	var modifyStatus = document.querySelector("#dictModifyStatus");
+	modifyStatus.innerHTML = '';
+
 	var i = 0;
-	var key, keyIndex = -1;
-	var valNode, keyNode = '';
+	var keyBlock = -1;
+	var keyIndex = -1;
+	var valNode = '';
+	var keyNode = '';
 
 	//Find key index in key column
 	for (i = 1; i < keyColumn.childElementCount; i++) {
-		key = keyColumn.childNodes[i];
+		keyBlock = keyColumn.childNodes[i];
 
-		if (keyClass === key.childNodes[0].className.trim()) {
+		if (keyClass === keyBlock.childNodes[0].className.trim()) {
 			//found index of key
 			keyIndex = i;
 			break;
@@ -108,22 +127,30 @@ const updateKeyValue = () => {
 	if (keyIndex === -1) {
 
 		//Create key-value blocks
-        	var keyBlock = document.createElement('div');
+        	var newKeyBlock = document.createElement('div');
 		var valBlock = document.createElement('div');
-        	keyBlock.className += ' keyBlock';
+        	newKeyBlock.className += ' keyBlock';
 		valBlock.className += ' valueBlock';
 
 
         	//Create key-value nodes
         	keyNode = createNode(keyClass);
 		valNode = createNode(valClass);
-        	keyBlock.appendChild(keyNode);
+        	newKeyBlock.appendChild(keyNode);
 		valBlock.appendChild(valNode);
 
         
        	 	//Append key-value blocks to container
-        	keyColumn.appendChild(keyBlock);
+        	keyColumn.appendChild(newKeyBlock);
 		valColumn.appendChild(valBlock);
+		
+		//Add equivalent code line to output section
+		PrintCodeLine(`myDict["` + keyClass + `"] = "` + valClass + `"`);
+	
+		//Check if game is won
+		if(CheckIfWon()) {
+			onWon();
+		}
 
 		return;
 	}
@@ -131,17 +158,187 @@ const updateKeyValue = () => {
 	//Overwite value in dictionary
 	var val = valColumn.childNodes[keyIndex];
 
+	//Value not found
 	if (typeof val === 'undefined') {
-		console.log("Dictionary is malformed.");
+		modifyStatus.textContent = "Oops the dictionary is corrupted. Please refresh the page.";
 		return;
 	}
 	
 	valNode = createNode(valClass);
 	val.textContent = '';
 	val.appendChild(valNode);
+
+	//Add equivalent code to code output section
+	PrintCodeLine(`myDict["` + keyClass + `"] = "` + valClass + `"`);
+
+	if (CheckIfWon()) {
+		onWon();
+	}
 }
 
-const removeKeyValue = () => {
+//Remove key-value pair in the user dictionary if it exists
+const removeKey = () => {
+	clearGameStatus();
+
+	var keySelect = document.querySelector("#dictGameSelectRemoveKey");
+
+	var keyClass = keySelect.options[keySelect.selectedIndex].value.toLowerCase(); //key to remove
+	
+	var keyColumn = document.querySelector("#dictContainer #dictKeys");
+	var valColumn = document.querySelector("#dictContainer #dictValues"); 
+
+	//Clear any error messages
+	var removeStatus = document.querySelector("#dictRemoveStatus");
+	removeStatus.innerHTML = '';
+
+	var i = 0;
+	var keyBlock = -1;
+	var keyIndex = -1;
+
+	//Find key index in key column
+	for (i = 1; i < keyColumn.childElementCount; i++) {
+		keyBlock = keyColumn.childNodes[i];
+
+		if (keyClass === keyBlock.childNodes[0].className.trim()) {
+			//found index of key
+			keyIndex = i;
+			break;
+		}
+	}
+
+	//Key not found
+	if (keyIndex === -1) {
+		removeStatus.textContent = "The key \"" + (keyClass.charAt(0).toUpperCase() + keyClass.slice(1)) + "\" does not exist.";
+		return;
+	}
+
+	//Delete key
+	var key = keyColumn.childNodes[keyIndex];
+	keyColumn.removeChild(key);
+
+	//Delete value
+	var val = valColumn.childNodes[keyIndex];
+
+	//Value not found
+	if (typeof val === 'undefined') {
+		removeStatus.textContent = "Oops the dictionary is corrupted. Please refresh the page.";
+		return;
+	}
+
+	valColumn.removeChild(val);
+
+	PrintCodeLine(`del myDict["`+ keyClass + `"]`); 
+
+	//Check if game is won
+	if(CheckIfWon()) {
+		onWon();
+	}
 }
 
-export {GameSetup, GenerateDict};
+//Returns true if "Your Dictionary" matches "Target Dictionary", false otherwise
+const CheckIfWon = () => {
+	//Compare user dictionary keys to solution dictionary keys
+	var userDictKeys = document.querySelector("#dictContainer #dictKeys");
+	var solDictKeys = document.querySelector("#solDictContainer #dictKeys");
+
+	var userDictVals = document.querySelector("#dictContainer #dictValues");
+	var solDictVals = document.querySelector("#solDictContainer #dictValues"); 
+
+	var i = 0;
+
+	//Check if dictionary is valid
+	if ((userDictKeys.childElementCount !== solDictKeys.childElementCount) || 
+	    (userDictVals.childElementCount !== solDictVals.childElementCount) ||
+	    (userDictKeys.childElementCount !== userDictVals.childElementCount)||
+	    (solDictKeys.childElementCount !== solDictVals.childElementCount)) {
+
+		return false;
+	}
+
+	//Compare keys to values
+	for (i = 0; i < userDictKeys.childElementCount; i++) {
+		if (userDictKeys.childNodes[i].innerHTML !== solDictKeys.childNodes[i].innerHTML) {
+			return false;
+		}
+		if (userDictVals.childNodes[i].innerHTML !== solDictVals.childNodes[i].innerHTML) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+//Disable dictionary modification buttons and show play again button on win
+const onWon = () => {
+	//Set status
+	var gameStatus = document.querySelector("#gameStatus");
+	gameStatus.innerHTML = "Congratulations! <strong>myDict</strong> matches <strong>targetDict</strong>.";
+
+	//Show play again button
+	var playAgainButton = document.querySelector("#dictPlayAgain");
+	playAgainButton.style.visibility = "visible";
+
+	//Disable modify/remove buttons
+	var modifyKeySubmit = document.querySelector("#dictGameButtonModify");
+	var removeKeySubmit = document.querySelector("#dictGameButtonRemove");
+
+	var addKeySelect = document.querySelector("#dictGameSelectAddKey");
+	var addValSelect = document.querySelector("#dictGameSelectAddValue");
+	var removeKeySelect = document.querySelector("#dictGameSelectRemoveKey");
+
+
+	modifyKeySubmit.disabled = true;
+	removeKeySubmit.disabled = true;
+	addKeySelect.disabled = true;
+	addValSelect.disabled = true;
+	removeKeySelect.disabled = true;
+
+	//Remove click listeners
+	modifyKeySubmit.removeEventListener('click', modifyKeyValue);
+	removeKeySubmit.removeEventListener('click', removeKey);
+}
+
+//Clear all game status tags
+const clearGameStatus = () => {
+	var gameStatusTags = document.querySelectorAll("[id*='Status']");
+	var i = 0;
+	for (i = 0; i < gameStatusTags.length; i++) {
+		gameStatusTags[i].textContent = '';
+	}
+}
+
+
+//Add code line to output section
+const PrintCodeLine = codeLine => {
+	var codeOutput = document.querySelector("#dictCodeOutput");
+
+	//Clear code output section if line limit is exceeded
+	if (codeOutput.childElementCount > MAX_CODE_LINES) {
+		codeOutput.textContent = '';
+	}
+
+	var preTag = document.createElement('pre');
+	preTag.className = 'dictgame_pre';
+	
+	var codeTag = document.createElement('code');
+	codeTag.innerHTML = codeLine;
+
+	preTag.appendChild(codeTag);
+	codeOutput.appendChild(preTag);
+}
+
+
+
+//show/hide the code section
+const ChangeCodeVisibility = () => {
+	var code = document.querySelector("#dictCode");
+	if (getComputedStyle(code).visibility === "hidden") {
+		code.style.visibility = "visible";
+	}
+	else {
+		code.style.visibility = "hidden";
+	}
+}
+
+
+export {GameSetup, GenerateDict, CheckIfWon, GameRestart};
