26bba6ff34bd2c294c1ce962aecadd877e68b1d6
Sukhman Vig <sukhman.vig@mail.utoronto.ca>
2021-03-26 18:16:10 -0400

    Update DREAM-120

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,15 @@
-import React, { useState } from "react";
-import Axios from "axios";
+import React, { useState, Fragment } from "react";
+
+import PythonproblemRepository from './components/python-problem-repository';
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
-import LoginForm from "./components/LoginForm";
-import RegisterForm from "./components/RegisterForm";
-import Navbar from "./components/Navbar";
-import NavbarBottom from "./components/NavbarBottom";
-import Dashboard from "./components/dashboard";
-
 function App() {
-  const [pageStatus, setPageStatus] = useState("");
-  const [error, setError] = useState("");
-  const [user, setUser] = useState({ name: "", email: "" });
-
-  const register = (details) => {
-    Axios.post("/users", {
-      username: details.name,
-      email: details.email,
-      password: details.password,
-      password_2nd: details.password_2nd,
-    }).then((response) => {
-      if (response.data.message) {
-        //setLoginStatus(response.data.message);
-        setError(response.data.message);
-      } else {
-        setUser({
-          name: details.name,
-          email: details.email,
-        });
-        setError("");
-        setPageStatus("Logged_in");
-      }
-    });
-  };
-
-  const Login = (details) => {
-    Axios.post("/login", {
-      email: details.email,
-      password: details.password,
-    }).then((response) => {
-      if (response.data.message) {
-        //setLoginStatus(response.data.message);
-        setError(response.data.message);
-      } else {
-        setError("");
-        setPageStatus("Logged_in");
-        setUser({
-          name: details.name,
-          email: details.email,
-        });
-      }
-    });
-  };
-
-  const Logout = (details) => {
-    setUser({ name: "", email: "" });
-    setError("");
-    setPageStatus("");
-  };
-
-  function refreshPage() {
-    window.location.reload(false);
-  }
-  return (
-    <>
-      <Navbar setPageStatus={setPageStatus} pageStatus={pageStatus} />
-      {pageStatus === "Logged_in" ? <Redirect to="/" /> : ""}
-      <Switch>
-        <Route exact path="/">
-          <div className="welcome App">
-            <h2>
-              Welcome, <span> {user.name} </span>
-              <button onClick={Logout}> Logout </button>
-            </h2>
-          </div>
-        </Route>
-        <Route exact path="/login">
+  return(
+    <Route exact path="/python-problem-repository">
           <div>
-            <LoginForm Login={Login} error={error} setError={setError} />
+          <PythonproblemRepository/>
           </div>
-        </Route>
-        <Route exact path="/signup">
-          <div>
-            <RegisterForm
-              register={register}
-              error={error}
-              setError={setError}
-            />
-          </div>
-        </Route>
-        <Route path="/about.html" component={refreshPage}></Route>
-        <Route path="/dashboard" component={Dashboard}></Route>
-      </Switch>
-      <NavbarBottom />
-    </>
-  );
+      </Route>
+  )
 }
 
 export default App;
