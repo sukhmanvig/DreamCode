c26c6dc9313ca512415d414b436a51c27234a73c
Komal Kaler <komal.kaler@mail.utoronto.ca>
2021-03-26 18:05:56 -0400

    [DREAM-126] fixed compile errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
 import React, { useState, Fragment } from "react";
-
 import PythonLearningResources from './components/python-learning-resources';
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+
 function App() {
   return(
-    <Route exact path="/python-learning-resources">
-          <div>
-          <PythonLearningResources/>
-          </div>
-      </Route>
+    <PythonLearningResources/>
   )
 }
 
