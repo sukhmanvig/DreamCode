3537d62f4a566d2404fcc38346b178e118fbf7c4
Jayvin Chang <jayvin.chang@mail.utoronto.ca>
2021-04-05 04:26:01 +0000

    Change px units to vw in dashboard css

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -50,74 +50,62 @@ const Dashboard = () => {
                   value="DAILY CHALLENGE"
                 />
               </Link>
-              <br></br>
           </div>
           <h1>GAMES</h1>
           <div id="bottomhalf">
-              <Link to="/dashboard">
-                <input
+              <input
                   type="text"
                   name="search-bar"
                   className="search-bar"
                   placeholder="Search Bar"
-                />
-                <input
-                  type="image"
-                  name="submit"
-                  src="images/magn-glass.jpg"
-                  className="search-button"
-                  alt="submit"
-                />
-              </Link>
-              <br></br>
+              />
               <hr />
-              <div className="continue-game">
-                <Link to="/listsgame">
-                  <input
-                    type="submit"
-                    className="continue"
-                    value="Lists Game"
-                  />
-                </Link>
-          <Link to="/PythonProblemRepository">
-          <input type="submit" className="continue" value="Python Problems" />
-          </Link>
-              </div>
-        <div className="continue-game">
-                <Link to="/sort-code">
-                  <input type="submit" className="continue" value="Sorting" />
-                </Link>
-        </div>
               <div className="category">
                 <i className="arrow left"></i>
                 <div id="js-category-name">CATEGORY1</div>
                 <i className="arrow right"></i>
+                <br></br>
+                <div className="category-games">
+                    <Link to="/stack_game">
+                      <input type="submit" className="game" value="Stacks" />
+                    </Link>
+                    
+                    <Link to="/listsgame">
+                      <input
+                        type="submit"
+                        className="game"
+                        value="Lists Game"
+                      />
+                    </Link>
 
-                <Link to="/stack_game">
-                  <input type="submit" className="game" value="Stacks" />
-                </Link>
+                    <Link to="/treetravtut">
+                      <input
+                        type="submit"
+                        className="game"
+                        value="Tree Traversals"
+                      />
+                    </Link>
 
-                <Link to="/treetravtut">
-                  <input
-                    type="submit"
-                    className="game"
-                    value="Tree Traversals"
-                  />
-                </Link>
-
-                <Link to="/dictcode">
-                  <input
-                    type="submit"
-                    className="game"
-                    value="Python Dictionaries"
-                  />
-                </Link>
-                <Link to="/ifcode">
-                  <input type="submit" className="game" value="If Statements" />
-                </Link>
-                <Link to="/queue_game">
-                  <input type="submit" className="game" value="Queues" />
-                </Link>
+                    <Link to="/dictcode">
+                      <input
+                        type="submit"
+                        className="game"
+                        value="Python Dictionaries"
+                      />
+                    </Link>
+                    <Link to="/ifcode">
+                      <input type="submit" className="game" value="If Statements" />
+                    </Link>
+                    <Link to="/queue_game">
+                      <input type="submit" className="game" value="Queues" />
+                    </Link>
+                    <Link to="/sort-code">
+                      <input type="submit" className="game" value="Sorting" />
+                    </Link>
+                    <Link to="/PythonProblemRepository">
+                      <input type="submit" className="game" value="Python Problems" />
+                    </Link>
+              </div>
               </div>
             </div>
         </div>
