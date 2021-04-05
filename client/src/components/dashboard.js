2c3606db29e7842ee786ca5ef823bbdd20e300a9
Jayvin Chang <jayvin.chang@mail.utoronto.ca>
2021-04-05 00:07:17 +0000

    Removed sort by UI

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect, useState } from "react";
 import "../css/nav.css";
 import "../css/dashboard-style.css";
 import CategoryNav from './CategoryNav';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   const [leaderboard, setLeaderboard] = useState([]);
@@ -30,100 +31,95 @@ const Dashboard = () => {
         <div id="content">
           <header> Dashboard </header>
 
-          <form action="/Profile">
-            <input type="submit" id="profile" value="PROFILE" />
-          </form>
-          <div id="leaderboard">
-            LEADERBOARD
-            <ol>
-              {leaderboard.map((entry, i) => (
-                <li key={i}>{entry.username + " " + entry.points + "XP"}</li>
-              ))}
-            </ol>
+          <div id="tophalf">
+              <Link to="/Profile">
+                <input type="submit" id="profile" value="PROFILE" />
+              </Link>
+              <div id="leaderboard">
+                LEADERBOARD
+                <ol>
+                  {leaderboard.map((entry, i) => (
+                    <li key={i}>{entry.username + " " + entry.points + "XP"}</li>
+                  ))}
+                </ol>
+              </div>
+              <Link to="/shell">
+                <input
+                  type="submit"
+                  className="daily-challenge"
+                  value="DAILY CHALLENGE"
+                />
+              </Link>
+              <br></br>
           </div>
-          <form action="/shell">
-            <input
-              type="submit"
-              className="daily-challenge"
-              value="DAILY CHALLENGE"
-            />
-          </form>
-          <br></br>
           <h1>GAMES</h1>
-          <form action="/dashboard">
-            <input
-              type="text"
-              name="search-bar"
-              className="search-bar"
-              placeholder="Search Bar"
-            />
-            <input
-              type="image"
-              name="submit"
-              src="images/magn-glass.jpg"
-              className="search-button"
-              alt="submit"
-            />
-          </form>
-          <div className="dashdropdown">
-            SELECT OPTION
-            <div className="dropdown-elements">
-              <a href="/dashboard">Option 1</a>
-              <a href="/dashboard">Option 2</a>
-              <a href="/dashboard">Option 3</a>
+          <div id="bottomhalf">
+              <Link to="/dashboard">
+                <input
+                  type="text"
+                  name="search-bar"
+                  className="search-bar"
+                  placeholder="Search Bar"
+                />
+                <input
+                  type="image"
+                  name="submit"
+                  src="images/magn-glass.jpg"
+                  className="search-button"
+                  alt="submit"
+                />
+              </Link>
+              <br></br>
+              <hr />
+              <div className="continue-game">
+                <Link to="/listsgame">
+                  <input
+                    type="submit"
+                    className="continue"
+                    value="Lists Game"
+                  />
+                </Link>
+          <Link to="/PythonProblemRepository">
+          <input type="submit" className="continue" value="Python Problems" />
+          </Link>
+              </div>
+        <div className="continue-game">
+                <Link to="/sort-code">
+                  <input type="submit" className="continue" value="Sorting" />
+                </Link>
+        </div>
+              <div className="category">
+                <i className="arrow left"></i>
+                <div id="js-category-name">CATEGORY1</div>
+                <i className="arrow right"></i>
+
+                <Link to="/stack_game">
+                  <input type="submit" className="game" value="Stacks" />
+                </Link>
+
+                <Link to="/treetravtut">
+                  <input
+                    type="submit"
+                    className="game"
+                    value="Tree Traversals"
+                  />
+                </Link>
+
+                <Link to="/dictcode">
+                  <input
+                    type="submit"
+                    className="game"
+                    value="Python Dictionaries"
+                  />
+                </Link>
+                <Link to="/ifcode">
+                  <input type="submit" className="game" value="If Statements" />
+                </Link>
+                <Link to="/queue_game">
+                  <input type="submit" className="game" value="Queues" />
+                </Link>
+              </div>
             </div>
-          </div>
-          <h2>Sort by</h2>
-          <br></br>
-          <hr />
-          <div className="continue-game">
-            <form action="/listsgame">
-              <input
-                type="submit"
-                className="continue"
-                value="Lists Game"
-              />
-            </form>
-	    <form action="/PythonProblemRepository">
-	     <input type="submit" className="continue" value="Python Problems" />
-	    </form>
-          </div>
-	  <div className="continue-game">
-          	<form action="/sort-code">
-	        	  <input type="submit" className="continue" value="Sorting" />
-          	</form>
-	  </div>
-          <div className="category">
-            <i className="arrow left"></i>
-            <div id="js-category-name">CATEGORY1</div>
-            <i className="arrow right"></i>
-
-            <form action="/stack_game">
-              <input type="submit" className="game" value="Stacks" />
-            </form>
-
-            <form action="/treetravtut">
-              <input
-                type="submit"
-                className="game"
-                value="Tree Traversals"
-              />
-            </form>
-
-            <form action="/dictcode">
-              <input
-                type="submit"
-                className="game"
-                value="Python Dictionaries"
-              />
-            </form>
-            <form action="/ifcode">
-              <input type="submit" className="game" value="If Statements" />
-            </form>
-            <form action="/queue_game">
-              <input type="submit" className="game" value="Queues" />\
-            </form>
-          </div>
         </div>
       </body>
     </Fragment>
